5933e4490ba611eb8ae750217096f7f2a3842155
asp <sheikh.abdullah.566@gmail.com>
2022-04-27 16:04:55 +0500

    Abdullah: Added Music Corousel

--- artists.php
+++ artists.php
@@ -11,6 +11,7 @@ $db = new Database();
     <?php
     include 'components/file.php';
     ?>
+
     <style>
         .album-info {
             white-space: nowrap;
@@ -20,7 +21,7 @@ $db = new Database();
 
         /* Main aspect ratio of artist image*/
         .single-album>img {
-            width: 100%;
+            width: 100%;  
             height: 100%;
             object-fit: contain;
 
@@ -29,7 +30,23 @@ $db = new Database();
         .single-album-item {
             display: block;
             padding: 0;
-            min-width: 200px;
+            min-width: 250px;
+        }
+
+        @media screen and (max-width: 480px) {
+            .single-album-item {
+                display: block;
+                padding: 5px;
+                width: 90%;
+            }
+        }
+
+        @media screen and (max-width: 768px) {
+            .single-album-item {
+                display: block;
+                padding: 0;
+                min-width: 150px;
+            }
         }
     </style>
 </head>
@@ -48,41 +65,40 @@ $db = new Database();
     <!-- ##### Breadcumb Area End ##### -->
     <section class="album-catagory section-padding-100-0">
         <div class="container">
-                <div class="col-md-12">
-                    <div class="browse-by-catagories catagory-menu d-flex justify-content-center flex-wrap mb-70">
-                        <a href="#" data-filter="*">Browse All</a>
-                        <a href="#" data-filter=".a" class="active">A</a>
-                        <a href="#" data-filter=".b">B</a>
-                        <a href="#" data-filter=".c">C</a>
-                        <a href="#" data-filter=".d">D</a>
-                        <a href="#" data-filter=".e">E</a>
-                        <a href="#" data-filter=".f">F</a>
-                        <a href="#" data-filter=".g">G</a>
-                        <a href="#" data-filter=".h">H</a>
-                        <a href="#" data-filter=".i">I</a>
-                        <a href="#" data-filter=".j">J</a>
-                        <a href="#" data-filter=".k">K</a>
-                        <a href="#" data-filter=".l">L</a>
-                        <a href="#" data-filter=".m">M</a>
-                        <a href="#" data-filter=".n">N</a>
-                        <a href="#" data-filter=".o">O</a>
-                        <a href="#" data-filter=".p">P</a>
-                        <a href="#" data-filter=".q">Q</a>
-                        <a href="#" data-filter=".r">R</a>
-                        <a href="#" data-filter=".s">S</a>
-                        <a href="#" data-filter=".t">T</a>
-                        <a href="#" data-filter=".u">U</a>
-                        <a href="#" data-filter=".v">V</a>
-                        <a href="#" data-filter=".w">W</a>
-                        <a href="#" data-filter=".x">X</a>
-                        <a href="#" data-filter=".y">Y</a>
-                        <a href="#" data-filter=".z">Z</a>
-                        <a href="#" data-filter=".number">0-9</a>
-                    </div>
+            <div class="col-md-12">
+                <div class="browse-by-catagories catagory-menu d-flex justify-content-center flex-wrap mb-70">
+                    <a href="#" data-filter="*">Browse All</a>
+                    <a href="#" data-filter=".a" class="active">A</a>
+                    <a href="#" data-filter=".b">B</a>
+                    <a href="#" data-filter=".c">C</a>
+                    <a href="#" data-filter=".d">D</a>
+                    <a href="#" data-filter=".e">E</a>
+                    <a href="#" data-filter=".f">F</a>
+                    <a href="#" data-filter=".g">G</a>
+                    <a href="#" data-filter=".h">H</a>
+                    <a href="#" data-filter=".i">I</a>
+                    <a href="#" data-filter=".j">J</a>
+                    <a href="#" data-filter=".k">K</a>
+                    <a href="#" data-filter=".l">L</a>
+                    <a href="#" data-filter=".m">M</a>
+                    <a href="#" data-filter=".n">N</a>
+                    <a href="#" data-filter=".o">O</a>
+                    <a href="#" data-filter=".p">P</a>
+                    <a href="#" data-filter=".q">Q</a>
+                    <a href="#" data-filter=".r">R</a>
+                    <a href="#" data-filter=".s">S</a>
+                    <a href="#" data-filter=".t">T</a>
+                    <a href="#" data-filter=".u">U</a>
+                    <a href="#" data-filter=".v">V</a>
+                    <a href="#" data-filter=".w">W</a>
+                    <a href="#" data-filter=".x">X</a>
+                    <a href="#" data-filter=".y">Y</a>
+                    <a href="#" data-filter=".z">Z</a>
+                    <a href="#" data-filter=".number">0-9</a>
                 </div>
+            </div>
 
             <div class="oneMusic-albums">
-
                 <?php
                 $db->select('artists', '*');
                 while ($row = mysqli_fetch_assoc($db->res)) {
@@ -91,7 +107,7 @@ $db = new Database();
                         $firstLetters .= "$word[0] ";
                     }
                 ?>
-                    <a href="./artist.php?id=<?php echo $row['artist_id'] ?>" class="col-11 col-sm-4 col-md-3 col-lg-1 col-xl-1 single-album-item ms-4 <?php echo $firstLetters ?>">
+                    <a href="sub-show/artist_page.php?id=<?php echo $row['artist_id'] ?>" class="single-album-item ms-4 <?php echo $firstLetters ?>">
                         <div class="single-album">
                             <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['artist_photo']); ?>" alt="Artist Photo" style="object-fit: cover; min-width: 200px; height: 200px;">
                             <div class="album-info">
@@ -101,7 +117,10 @@ $db = new Database();
                         </div>
                     </a>
                 <?php } ?>
+
             </div>
+
+
         </div>
     </section>
 
--- sub-show/artist_page.php
+++ sub-show/artist_page.php
@@ -8,6 +8,8 @@ if (isset($_GET['id'])) {
     $artist = mysqli_fetch_assoc($db->res);
     $db->select('albums', '*', "album_artist = $id ORDER BY album_year DESC");
     $albums = $db->res;
+    $db->select('music', '*', "music_artist = $id ORDER BY music_year DESC");
+    $music = $db->res;
 }
 ?>
 
@@ -23,6 +25,7 @@ if (isset($_GET['id'])) {
             margin: auto;
             display: block;
         }
+<<<<<<< Updated upstream
     .photo>img {
       border: 5px solid #efefef;
     }
@@ -31,29 +34,42 @@ if (isset($_GET['id'])) {
     }
 
    
+=======
+
+        .photo>img {
+            border: 5px solid #efefef;
+        }
+
+        .text {
+            text-align: center;
+        }
+
+        .col-md-4 {
+            border: 1px solid black;
+        }
+>>>>>>> Stashed changes
     </style>
 </head>
 
 <body>
 
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/flow/components/nav.php' ?>
-<div style="height: 100px;"></div>
+    <div style="height: 100px;"></div>
     <div class="container my-5 p-10">
         <div class="row">
             <div class="col-12 col-md-4">
-            <div class="photo m-3">
+                <div class="photo m-3">
                     <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($artist['artist_photo']); ?>" class="rounded-circle" alt="Artist Photo" style="object-fit: cover;" />
                     <h2 class="text"><?php echo $artist['artist_name'] ?></h2>
                 </div>
-
             </div>
 
             <div class="col-12 col-md-8">
-            <h3 class="text-center mb-4">L A T E S T &nbsp; A L B U M S</h3>
-            <section class="latest-albums-area" data-aos="fade-up" data-aos-duration="3000">
-            <div class="albums-slideshow owl-carousel">
+                <h3 class="text-center mb-4">L A T E S T &nbsp; A L B U M S</h3>
+                <section class="latest-albums-area" data-aos="fade-up" data-aos-duration="3000">
+                    <div class="albums-slideshow owl-carousel">
 
-            <?php while ($row = mysqli_fetch_assoc($albums)) { ?>
+                        <?php while ($row = mysqli_fetch_assoc($albums)) { ?>
                             <div class="single-album">
                                 <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['album_thumbnail']); ?>" alt="Album Photo" style="height:170px" />
                                 <div class="album-info">
@@ -64,20 +80,32 @@ if (isset($_GET['id'])) {
                                 </div>
                             </div>
                         <?php } ?>
-            </div>
-            </section>
+                    </div>
+                </section>
 
-            <!----FOR MUSIC ----->
-            <div>
-            <h3 class="text-center mb-4">L A T E S T &nbsp; MUSIC</h3>
-            </div>
+                <h3 class="text-center mb-4">L A T E S T &nbsp; M U S I C</h3>
+                <section class="latest-albums-area" data-aos="fade-up" data-aos-duration="3000">
+                    <div class="albums-slideshow owl-carousel">
 
+                        <?php while ($row = mysqli_fetch_assoc($music)) { ?>
+                            <div class="single-album">
+                                <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['music_thumbnail']); ?>" alt="Music Photo" style="height:170px" />
+                                <div class="album-info">
+                                    <a href="#">
+                                        <h5 class="text-dark"><?php echo $row['music_title'] ?></h5>
+                                    </a>
+                                    <p><?php echo $row['music_year'] ?></p>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </section>
 
             </div>
 
         </div>
 
-        </div>
+    </div>
 
     </div>
 
